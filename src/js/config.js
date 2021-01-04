af8a5498ec76a4a94a4a9b997564146e689e2d5c
Ans khan <anskhilji900@gmail.com>
2021-01-04 11:31:26 +0500

    inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2b927a31-9d3a-4019-bc34-95870f4dbbde';
+export const MODEL_CLOSE_SEC = 2.5;
